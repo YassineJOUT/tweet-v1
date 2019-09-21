8d7bf15c4b5cd3ffbaa3e8b7798f20e6afdf854e
Yassine JOUT <yassinejout@gmail.com>
2019-09-21 16:28:33 +0100

    Adding Nav bar to ui

---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import  '../css/Nav.css'
+
+const Nav = ({name}) => {
+    return (
+        <div className="nav">
+                <ul>
+                    <li className="app-logo">
+                        <a href="/Home">{name}</a>
+                    </li>
+                    <li>
+                        <a href="/Home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/Info">Info</a>
+                    </li>
+                    <li>
+                        <a href="/About">About</a>
+                    </li>
+                </ul>
+        </div>
+    );
+};
+
+export default Nav;
